368ee5ba6a4e64a334d4b52ec284a5c8de88faea
Dave <none@example.com>
2013-06-28 17:33:05 -0700

    Created first classroom

--- spot/classroom/models.py
+++ spot/classroom/models.py
@@ -1,3 +1,87 @@
-from django.db import models
+from __future__ import division
+from __future__ import unicode_literals
 
-# Create your models here.
+import copy
+
+from django.contrib.auth.models import User
+from django.db.models import *
+
+
+class ClassroomInstructor(Model):
+    user = ForeignKey(User)
+    public_name = CharField(max_length=200, blank=True)
+    office_location = CharField(max_length=200, blank=True)
+    office_hours = CharField(max_length=200, blank=True)
+    email = EmailField(blank=True)
+    website = URLField(blank=True)
+
+    def get_public_name(self):
+        if self.public_name:
+            return self.public_name
+        else:
+            return self.user.get_full_name()
+        
+    def __unicode__(self):
+        return self.get_public_name()
+
+    class Meta:
+        ordering = ['user__last_name', 'user__first_name']
+
+
+class Classroom(Model):
+    is_active = BooleanField(default=True)
+    slug = SlugField()
+    first_day = DateField()
+    dept = CharField(max_length=200)
+    term = CharField(max_length=200)
+    
+    subtitle = CharField(max_length=200, blank=True)
+    overview = TextField(blank=True)
+    outline = TextField(blank=True)
+
+    instructor = ForeignKey(ClassroomInstructor, null=True, blank=True)
+    scratchpad = TextField(blank=True)
+
+    @property
+    def title(self):
+        return '{self.dept} {self.term}'.format(self=self)
+    @property
+    def year(self):
+        return self.first_day.year
+
+    @property
+    def season(self):
+        return ['Winter','Spring','Summer','Fall'][int(self.first_day.month / 3)]
+        
+    def copy_instance(self):
+        instance = copy.deepcopy(self)
+        instance.id = None
+        instance.save()
+
+    def __unicode__(self):
+        x = '{self.title}, {self.season} {self.year}'.format(self=self)
+        if not self.is_active:
+            x += ' [Inactive]'
+        return x
+
+    class Meta:
+        ordering = ['-first_day']
+
+        
+class ClassroomStudent(Model):
+    classroom = ForeignKey(Classroom)
+    user = ForeignKey(User)
+        
+    @property
+    def last_name(self):
+        return self.user.last_name
+
+    @property
+    def first_name(self):
+        return self.user.first_name
+        
+    def __unicode__(self):
+        return '{self.last_name}, {self.first_name} in {self.classroom}'.format(self=self)
+
+    class Meta:
+        ordering = ['user__last_name', 'user__first_name']
