e480f7e165fc55d24d0f80b5d2715a38c136820e
dave <none@example.com>
2013-07-01 13:50:39 -0700

    Setting up edit form

--- apps/classroom/urls.py
+++ apps/classroom/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.list_classrooms, name='list_classrooms'),
     url(r'^(\d+)/$', views.show_classroom, name='show_classroom'),
+    url(r'^(\d+)/edit/$', views.edit_classroom, name='edit_classroom'),
+    url(r'^(\d+)/post/$', views.post_classroom, name='post_classroom'),
 )
