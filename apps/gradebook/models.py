df3d940c2808ad831eb9d403a0bb4b22a7330394
dulrich15 <davidjulrich@gmail.com>
2013-07-08 17:01:37 -0700

    Fixed bug in grade edit; added grade weight model

--- apps/gradebook/models.py
+++ apps/gradebook/models.py
@@ -7,10 +7,10 @@ from apps.classroom.models import Classroom, Student
 
 
 class AssignmentCategory(Model):
-    name = CharField(max_length=200)
+    label = CharField(max_length=200)
 
     def __unicode__(self):
-        return self.name
+        return self.label
 
     class Meta:
         verbose_name_plural = 'assignment categories'
@@ -31,7 +31,16 @@ class Assignment(Model):
     
     @property
     def grades(self):
-        return AssignmentGrade.objects.filter(assignment=self).order_by('student')
+        grades = []
+        for student in Student.objects.filter(classroom=self.classroom):
+            try:
+                grade = AssignmentGrade.objects.get(assignment=self, student=student)
+            except:
+                class grade(object):
+                    pass
+            grade.student = student
+            grades.append(grade)
+        return grades
     
     def __unicode__(self):
         return '{self.label} in {self.classroom}'.format(self=self)
@@ -63,51 +72,25 @@ class AssignmentGrade(Model):
         return u'{self.assignment.label} for {self.student}'.format(self=self)
 
 
-# class AssignmentGradeWeight(Model):
-#     course = ForeignKey(Course)
-#     title = CharField(max_length=200)
-#     weight_raw = PositiveSmallIntegerField(default=1)
-#
-#     # gradebook = ForeignKey(Gradebook)
-#     # title = CharField(max_length=20)
-#     # abbr = CharField(max_length=2)
-#     # weight = DecimalField(max_digits=2,decimal_places=2,help_text='Enter percent as decimal: e.g., for 20% type "0.20"')
-#     # subtitle = CharField(max_length=200,blank=True)
-#     # drop_worst = BooleanField(default=False)
-#     # drop_best = BooleanField(default=False)
-#     # if_missing_use = ForeignKey('AssignmentGradeWeight',null=True,blank=True,related_name='*')
-#
-#     # def csv_data(self):
-#         # return '{0},{1},{2}'.format(self.abbr, self.title, self.weight)
-#
-#     # def __unicode__(self):
-#         # label = '{0}'.format(self.title)
-#         # if self.subtitle:
-#             # label += ' [{0}]'.format(self.subtitle)
-#         # label += ' at {0:.0%}'.format(self.weight)
-#
-#         # if self.drop_worst:
-#             # label += ' (drop worst)'
-#         # if self.drop_best:
-#             # label += ' (drop best)'
-#         # if self.if_missing_use:
-#             # label += ' (if missing use %s)' % self.if_missing_use
-#         # label = label.replace(') (',', ')
-#         # return label
-#
-#     # class Meta:
-#         # ordering = ['gradebook','-weight','drop_worst','drop_best']
-#         # verbose_name_plural = 'assignment categories'
-#
-#     def weight(self):
-#         o = self.__class__.objects.filter(course=self.course)
-#         tot = sum([gw.weight_raw for gw in o])
-#         return self.weight_raw / tot
-#
-#     def __unicode__(self):
-#         return '{0} at {1:.0%}'.format(self.title, self.weight())
-#
-#     class Meta:
-#         ordering = ['course', '-weight_raw']
-#         # verbose_name_plural = 'assignment categories'
+class AssignmentGradeWeight(Model):
+    classroom = ForeignKey(Classroom)
+    category = ForeignKey(AssignmentCategory)
+    weight_raw = PositiveSmallIntegerField(default=1)
+    
+    # if_missing_use = ForeignKey('AssignmentGradeWeight',null=True,blank=True,related_name='*')
+    # drop_worst = BooleanField(default=False)
+    # drop_best = BooleanField(default=False)
+    # notes = TextField(null=True, blank=True)
+
+    @property
+    def weight(self):
+        o = self.__class__.objects.filter(classroom=self.classroom)
+        tot = sum([gw.weight_raw for gw in o])
+        return self.weight_raw / tot
+
+    def __unicode__(self):
+        return '{self.category.label} at {self.weight:.0%}'.format(self=self)
+
+    class Meta:
+        ordering = ['classroom', '-weight_raw', 'category']
 
